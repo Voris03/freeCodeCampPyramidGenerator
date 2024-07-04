abc3e2ba88c2a68931b5bf894d7d4278f85b19b8
Voris03 <v.vidus03@gmail.com>
2024-07-05 02:46:29 +0300

    add: step 27

--- script.js
+++ script.js
@@ -13,5 +13,7 @@ console.log("#########");
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-rows.push("freeCodeCamp")
-console.log(rows[0]);
+rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(rows);
+console.log(popped);
