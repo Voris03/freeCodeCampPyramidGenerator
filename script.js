c7bb3424d6b66d835ef3f8ed7ecf23b08bd21b6e
Voris03 <v.vidus03@gmail.com>
2024-07-05 02:24:22 +0300

    add: step 21

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ console.log(" ####### ");
 console.log("#########");
 
 
-//Step 2-20
+//Step 2-21
 
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 console.log(rows[0]);
+rows[2] = 10;
+console.log(rows);
