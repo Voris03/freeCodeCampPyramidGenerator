e3a3009c4cd216e1e5e0899ea9035231bd380c27
Voris03 <v.vidus03@gmail.com>
2024-07-05 00:28:05 +0300

    add: step 2-3

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Step 1
+
 console.log("Let's build a pyramid using JavaScript basics:");
 console.log("    #    ");
 console.log("   ###   ");
 console.log("  #####  ");
 console.log(" ####### ");
 console.log("#########");
+
+
+//Step 2-3
+
+let character = "Hello";
