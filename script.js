ea60e8eb6efa6eaf94fb8886b33a8ec6f9079892
Voris03 <v.vidus03@gmail.com>
2024-07-05 18:13:34 +0300

    add: step 66

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function padRow(rowNumber, rowCount) {
 
 
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(character.repeat(i+1));
+    rows.push(padRow())
 }
 let result = "";
 console.log(result)
