86e789ef1d132004db648da24e47367ff5f8e9b3
Voris03 <v.vidus03@gmail.com>
2024-07-05 04:00:08 +0300

    add: step 34

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ console.log("#########");
 const character = "#";
 const count = 8;
 const rows = [];
-for ("iterator"; "condition"; "iteration") {
+for (let i = 0; "condition"; "iteration") {
 
 }
