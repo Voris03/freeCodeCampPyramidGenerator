15221ff0df1cfdabd8a578e8794eff4cc5321f52
Voris03 <v.vidus03@gmail.com>
2024-07-05 06:27:12 +0300

    add: step 60

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ const rows = [];
 
 function padRow (name) {
     const test = "Testing";
+    
+    console.log("This works!");
     return test;
+    console.log("This works!");
 }
 const call = padRow("Voris");
 console.log(call);
