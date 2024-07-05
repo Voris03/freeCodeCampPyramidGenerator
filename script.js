e40d1c811710f122399a42a5281b54b7cad6c167
Voris03 <v.vidus03@gmail.com>
2024-07-05 04:59:50 +0300

    add: step 44

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ console.log("#########");
 const character = "#";
 const count = 8;
 const rows = [];
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(i)
-}
+for (let character = 0; character < count; character = character + 1) {
+    rows.push(character);
+  }
 let result = "";
 console.log(result)
 
