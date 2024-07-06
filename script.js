18a6cd792a72835ff1f60aef8d851432cdb33c49
Voris03 <v.vidus03@gmail.com>
2024-07-06 22:19:42 +0300

    add: step 117

--- script.js
+++ script.js
@@ -30,23 +30,6 @@ for (let i = 1; i <= count; i++) {
     }
 } 
 
-// while (rows.length < count) {
-//     rows.push(padRow(rows.length + 1))
-// }
-
-// for (let i = count; i > 0; i-- ){
-//     rows.push(padRow(i, count))
-// }
-
-// Delete 
-/*const numbers = [1, 2, 3];
-const shifted = numbers.shift()
-
-const unshifted = numbers.unshift(5);
-console.log(shifted);
-console.log(unshifted);
-console.log(numbers) */
-
 let result = "";
 console.log(result)
 
